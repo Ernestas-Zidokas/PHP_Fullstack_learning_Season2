0640bc140c027a92f6792266cad7b265ae424a66
Katamaranas <ernestas.zidokas@gmail.com>
2019-03-19 11:59:36 +0200

    trecia uzduotis class

--- index.php
+++ index.php
@@ -5,12 +5,21 @@ Class ThailandSurprise {
     public $clothes;
     private $balls;
 
-    function __construct() {
+    public function __construct() {
         $this->balls = rand(0, 1);
+    }
+    
+    public function attachBalls(){
+        $this->balls = true;
+    }
+    
+    public function detachBalls(){
+       $this->balls = false; 
     }
 
 }
 
 $surprise = new ThailandSurprise();
 
+print $surprise->clothes = 'miniskirt';
 ?>
