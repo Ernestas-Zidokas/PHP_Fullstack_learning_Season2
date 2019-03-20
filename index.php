8fa20a660402b574a401321317fc3a7a26a750fb
Katamaranas <ernestas.zidokas@gmail.com>
2019-03-20 12:39:30 +0200

    1ma uzduotis treciadienis strict parametrai

--- index.php
+++ index.php
@@ -1,44 +1,49 @@
 <?php
+declare (strict_types = 1);
 
-Class Jacuzzi {
+Class Gerimas {
 
-    public $amount_water;
-    public $amount_non_water;
+    private $data;
 
-    public function __construct($amount_water = 0, $amount_non_water = 0) {
-        $this->amount_water = $amount_water;
-        $this->amount_non_water = $amount_non_water;
+    public function __construct() {
+        $this->data = [
+            'name' => null,
+            'amount_ml' => null,
+            'abarot' => null
+        ];
     }
 
-    public function getWaterPurity() {
-        return $this->amount_water / ($this->amount_water + $this->amount_non_water) * 100;
+    public function setName(string $name) {
+        $this->data['name'] = $name;
+    }
+
+    public function setAmount(int $amount) {
+        $this->data['amount_ml'] = $amount;
+    }
+
+    public function setAbarot(float $abarot) {
+        $this->data['abarot'] = $abarot;
+    }
+
+    public function getName() {
+        return $this->data['name'];
+    }
+
+    public function getAmount() {
+        return $this->data['amount_ml'];
+    }
+
+    public function getAbarot() {
+        return $this->data['abarot'];
     }
 
 }
-
-Class User {
-
-    public function peeInJacuzzi(Jacuzzi $jacuzzi, $amount) {
-        $jacuzzi->amount_non_water += $amount;
-    }
-
-}
-
-$pliuskupliusku = new Jacuzzi(600);
-$petras = new User();
-$piotra = new User();
-$petras->peeInJacuzzi($pliuskupliusku, rand(0, 200) / 1000);
-$piotra->peeInJacuzzi($pliuskupliusku, rand(0, 100) / 1000);
-
-$pliuskupliusku->getWaterPurity();
 ?>
 <html>
     <head>
         <title>OOP</title>
     </head>
     <body>
-        <?php print 'Skaidrus vanduo: ' . $pliuskupliusku->amount_water; ?>
-        <?php print 'Neskaidrus vanduo: ' . $pliuskupliusku->amount_non_water; ?>
-        <?php print 'Vandens skaidrumas: ' . $pliuskupliusku->getWaterPurity(); ?>
+
     </body>
 </html>
