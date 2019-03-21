694ee390e72203091ca32e2f5d2b12bc92156762
Katamaranas <ernestas.zidokas@gmail.com>
2019-03-21 10:18:21 +0200

    aprasytos klases naudojimui

--- index.php
+++ index.php
@@ -5,12 +5,16 @@ Class Gerimas {
 
     private $data;
 
-    public function __construct() {
-        $this->data = [
-            'name' => null,
-            'amount_ml' => null,
-            'abarot' => null
-        ];
+    public function __construct($data = null) {
+        if (!$data) {
+            $this->data = [
+                'name' => null,
+                'amount_ml' => null,
+                'abarot' => null
+            ];
+        } else {
+            $this->setData($data);
+        }
     }
 
     public function setName(string $name) {
@@ -36,19 +40,71 @@ Class Gerimas {
     public function getAbarot() {
         return $this->data['abarot'];
     }
-    
-    public function setData(array $data){
-        $data = [
-            'name' => $this->setName(),
-            'amount' => $this->setAmount(),
-            'abarot' => $this->setAbarot()
-        ];
+
+    public function setData(array $data) {
+        $this->setName($data['name'] ?? null);
+        $this->setAmount($data['amount'] ?? null);
+        $this->setAbort($data['abort'] ?? null);
     }
 
     public function getData() {
         return $this->data;
     }
 
+}
+
+Class FileDb {
+
+    private $file_uri;
+    private $data;
+
+    public function __construct($file_uri) {
+        $this->file_uri = $file_uri;
+        $this->data = null;
+        $this->load();
+    }
+
+    public function setRow($table, $row_id, $row_data) {
+        $this->data[$table][$row_id] = $row_data;
+    }
+
+    public function setRowColumn($table, $row_id, $column_id, $column_data) {
+        $this->data[$table][$row_id][$column_id] = $column_data;
+    }
+
+    public function getRow($table, $row_id) {
+        return $this->data[$table][$row_id];
+    }
+
+    public function getRowColumn($table, $row_id, $column_id) {
+        return $this->data[$table][$row_id][$column_id];
+    }
+
+    public function load() {
+        if (!file_exists($this->file_uri)) {
+            $this->data = [];
+        } else {
+            $json_data = file_get_contents($this->file_uri);
+            $this->data = json_decode($json_data, true);
+        }
+    }
+
+    public function save() {
+        $data_json = json_encode($this->data);
+        if (file_put_contents($this->file_uri, $data_json)) {
+            return true;
+        } else {
+            throw new Exception('Neisejo issaugoti i faila.');
+        }
+    }
+
+}
+
+Class ModelGerimai {
+
+    public function __construct(FileDb $db, $table_name) {
+        
+    }
 }
 ?>
 <html>
